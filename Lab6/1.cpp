84ac9f521e5202e60e34f13a1ba4dd6dc4711b7b
Rokku04 <enheep995@gmail.com>
2026-04-08 23:24:56 +0800

    sq

--- Lab6/1.cpp
+++ Lab6/1.cpp
@@ -99,7 +99,7 @@ private:
 
 public:
     // leftTop = зүүн дээд орой
-    Square(string n, double x, double y, double s) : TwoDshape(n){
+    Square(string n, double x, double y, double s) : TwoDshape(n, "White"){
         side = s;
 
         // Зүүн дээд орой
